6f041c6cc9a45115f6375fe21bd5be6621620366
raghavsharma677 <81424060+raghavsharma677@users.noreply.github.com>
2021-07-28 16:52:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -50,3 +50,17 @@ Webcam.set({
     jpeg_quality:90
 });
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
